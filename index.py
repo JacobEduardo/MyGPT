d10bd350a4174b4420136cd66b1ef1c0181def21
JacobEduardo <jacob.parraguez.vargas@gmail.com>
2023-04-25 10:42:14 -0400

    Frist commit

---
+++ index.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+import openai
+
+load_dotenv()
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+openai.Completion.create(
+  model="text-davinci-003",
+  prompt="Say this is a test",
+  max_tokens=7,
+  temperature=0
+)
